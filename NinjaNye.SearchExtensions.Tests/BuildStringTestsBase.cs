8406db04f3111cf9bc05cb96d4781e569437a899
axeeck <axeeck@pixel.de>
2016-10-21 12:59:41 +0200

    Improfed BuildStringTestBase by generating words in parallel.

--- NinjaNye.SearchExtensions.Tests/BuildStringTestsBase.cs
+++ NinjaNye.SearchExtensions.Tests/BuildStringTestsBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -9,29 +10,29 @@ namespace NinjaNye.SearchExtensions.Tests
     {
         private const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-        protected IList<string> BuildWords(int wordCount, int minSize = 2, int maxSize = 10)
+        protected IList<string> BuildWords(int wordCount, int minSize = 2, int maxSize = 10, bool processParallel = true)
         {
             Console.WriteLine("Building {0} words...", wordCount);
-            var result = new List<string>();
-            for (int i = 0; i < wordCount; i++)
-            {
-                string randomWord = BuildRandomWord(minSize, maxSize);
-                result.Add(randomWord);
-            }
-            Console.WriteLine("Built words: {0}", wordCount);
+
+			var query = Enumerable.Range(0, wordCount);
+			if (processParallel) query = query.AsParallel();
+
+			var result = query
+				.Select(x => BuildRandomWord(minSize, maxSize, false))
+				.ToList();
+
+			Console.WriteLine("Built words: {0}", wordCount);
             return result;
         }
 
-        protected string BuildRandomWord(int minSize, int maxSize)
+        protected string BuildRandomWord(int minSize, int maxSize, bool processParallel = true)
         {
-            var letterCount = RandomInt(minSize, maxSize);
-            var sb = new StringBuilder(letterCount);
-            for (int i = 0; i < letterCount; i++)
-            {
-                var letterIndex = RandomInt(0, 51);
-                sb.Append(letters[letterIndex]);
-            }
-            return sb.ToString();
+			var letterCount = RandomInt(minSize, maxSize);
+
+			var query = Enumerable.Range(0, letterCount);
+			if (processParallel) query = query.AsParallel();
+
+			return new string(query.Select(x => letters[RandomInt(0, 51)]).ToArray());
         }
 
         private int RandomInt(int min, int max)
