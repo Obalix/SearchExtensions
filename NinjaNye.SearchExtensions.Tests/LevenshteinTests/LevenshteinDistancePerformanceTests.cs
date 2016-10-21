238d02f817e62b8e138ffa59d1f114a40f965d64
axeeck <axeeck@pixel.de>
2016-10-21 12:49:46 +0200

    Changed to version NUnit 3.0 and VS

--- NinjaNye.SearchExtensions.Tests/LevenshteinTests/LevenshteinDistancePerformanceTests.cs
+++ NinjaNye.SearchExtensions.Tests/LevenshteinTests/LevenshteinDistancePerformanceTests.cs
@@ -7,32 +7,31 @@ using NinjaNye.SearchExtensions.Levenshtein;
 namespace NinjaNye.SearchExtensions.Tests.LevenshteinTests
 {
 #if DEBUG
-    // Performance tests will likely fail in debug mode
-    [Ignore]
+	[Ignore("Performance tests will likely fail in debug mode")]
 #endif
-    [TestFixture]
-    public class LevenshteinDistancePerformanceTests : BuildStringTestsBase
-    {
-        [TestCase(6)]
-        [TestCase(7)]
-        public void ToLevenshteinDistance_CompareOneMillionStringsOfLengthX_ExecutesInLessThanOneSecond(int length)
-        {
-            //Arrange
-            var words = BuildWords(1000000, length, length);
-            var randomWord = BuildRandomWord(length,length);
-            var stopwatch = new Stopwatch();
-            //Act
-            stopwatch.Start();
-            var result = words.Select(w => LevenshteinProcessor.LevenshteinDistance(w, randomWord)).ToList();
-            stopwatch.Stop();
+	[TestFixture]
+	public class LevenshteinDistancePerformanceTests : BuildStringTestsBase
+	{
+		[TestCase(6)]
+		[TestCase(7)]
+		public void ToLevenshteinDistance_CompareOneMillionStringsOfLengthX_ExecutesInLessThanOneSecond(int length)
+		{
+			//Arrange
+			var words = BuildWords(1000000, length, length);
+			var randomWord = BuildRandomWord(length,length);
+			var stopwatch = new Stopwatch();
+			//Act
+			stopwatch.Start();
+			var result = words.Select(w => LevenshteinProcessor.LevenshteinDistance(w, randomWord)).ToList();
+			stopwatch.Stop();
 
-            //Assert
-            Console.WriteLine("Elapsed Time: {0}", stopwatch.Elapsed);
-            Console.WriteLine("Total matching words: {0}", result.Count(i => i == 0));
-            Console.WriteLine("Total words with distance of 1: {0}", result.Count(i => i == 1));
-            Console.WriteLine("Total words with distance of 2: {0}", result.Count(i => i == 2));
-            Console.WriteLine("Total words with distance of 3: {0}", result.Count(i => i == 3));
-            Assert.That(stopwatch.Elapsed.TotalMilliseconds, Is.LessThan(1000));
-        }
-    }
+			//Assert
+			Console.WriteLine("Elapsed Time: {0}", stopwatch.Elapsed);
+			Console.WriteLine("Total matching words: {0}", result.Count(i => i == 0));
+			Console.WriteLine("Total words with distance of 1: {0}", result.Count(i => i == 1));
+			Console.WriteLine("Total words with distance of 2: {0}", result.Count(i => i == 2));
+			Console.WriteLine("Total words with distance of 3: {0}", result.Count(i => i == 3));
+			Assert.That(stopwatch.Elapsed.TotalMilliseconds, Is.LessThan(1000));
+		}
+	}
 }
