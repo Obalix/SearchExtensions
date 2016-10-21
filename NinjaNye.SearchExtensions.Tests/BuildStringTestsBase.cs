da74fac7c2640a7f8c436adad9768412317d3d8b
Obalix <axel.eckenberger@gmail.com>
2016-10-21 13:36:50 +0200

    Moved code from NinjaNye.SearchExtensions to shared project, as preparation for PCL implementation.

--- NinjaNye.SearchExtensions.Tests/BuildStringTestsBase.cs
+++ NinjaNye.SearchExtensions.Tests/BuildStringTestsBase.cs
@@ -6,13 +6,13 @@ using System.Text;
 
 namespace NinjaNye.SearchExtensions.Tests
 {
-    public class BuildStringTestsBase
-    {
-        private const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	public class BuildStringTestsBase
+	{
+		private const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-        protected IList<string> BuildWords(int wordCount, int minSize = 2, int maxSize = 10, bool processParallel = true)
-        {
-            Console.WriteLine("Building {0} words...", wordCount);
+		protected IList<string> BuildWords(int wordCount, int minSize = 2, int maxSize = 10, bool processParallel = true)
+		{
+			Console.WriteLine("Building {0} words...", wordCount);
 
 			var query = Enumerable.Range(0, wordCount);
 			if (processParallel) query = query.AsParallel();
@@ -22,26 +22,26 @@ namespace NinjaNye.SearchExtensions.Tests
 				.ToList();
 
 			Console.WriteLine("Built words: {0}", wordCount);
-            return result;
-        }
+			return result;
+		}
 
-        protected string BuildRandomWord(int minSize, int maxSize)
-        {
+		protected string BuildRandomWord(int minSize, int maxSize)
+		{
 			var letterCount = RandomInt(minSize, maxSize);
 			var wordLettes = Enumerable.Range(0, letterCount)
 				.Select(x => letters[RandomInt(0, 51)]);
 			return new string(wordLettes.ToArray());
-        }
+		}
 
-        private int RandomInt(int min, int max)
-        {
-            var rng = new RNGCryptoServiceProvider();
-            var buffer = new byte[4];
+		private int RandomInt(int min, int max)
+		{
+			var rng = new RNGCryptoServiceProvider();
+			var buffer = new byte[4];
 
-            rng.GetBytes(buffer);
-            int result = BitConverter.ToInt32(buffer, 0);
+			rng.GetBytes(buffer);
+			int result = BitConverter.ToInt32(buffer, 0);
 
-            return new Random(result).Next(min, max);
-        }
-    }
+			return new Random(result).Next(min, max);
+		}
+	}
 }
